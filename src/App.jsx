7b916a992afcf52a6ccbb9d96a19a1d1f7881510
DerrickWells <derrickwellsjr@gmail.com>
2025-09-01 00:05:57 -0400

    add redux to firebase hosting init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="content">
+      <h1>Welcome to Datical Design</h1>
+<p>Currently under construction.</p>
+<p>For information, please email derrickwells@daticaldesign.com</p>
+    </div>
+  );
+};
+
+export default App;
